ff8442eed5530f1506d687aa29dd776f5708e90b
YbicG <82431866+YbicG@users.noreply.github.com>
2023-08-18 19:29:55 -0500

    Create track.py

---
+++ track.py
@@ -0,0 +1,367 @@
+def run():
+    import discord
+    import json
+    import time
+    import requests, requests.auth
+    from config import CONFIG
+
+    intents = discord.Intents.all()
+    bot = discord.Client(intents=intents)
+    tree = discord.app_commands.CommandTree(bot)
+
+    # Class of different styles
+    class style():
+        BLACK = '\033[30m'
+        RED = '\033[31m'
+        GREEN = '\033[32m'
+        YELLOW = '\033[33m'
+        BLUE = '\033[34m'
+        MAGENTA = '\033[35m'
+        CYAN = '\033[36m'
+        WHITE = '\033[37m'
+        UNDERLINE = '\033[4m'
+        NONE = '\033[0m'
+
+    def get_package(tracking_number=" "):
+        tracking_number = tracking_number.replace(" ", "")
+
+        url = "https://api.ship24.com/public/v1/tracking/search"
+
+        payload = {"trackingNumber": tracking_number}
+        headers = {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Authorization": "apik_O6Y0Ybjz5kkXBajM9MGepQKhCxhI5p",
+        }
+
+        if tracking_number != " " and tracking_number != None:
+            package = requests.post(url=url, headers=headers, json=payload)
+
+            if "errors" in json.loads(package.text):
+                print(f"{style.RED}[ERROR]{style.NONE} {style.MAGENTA} Tracking Number: "+str(tracking_number)+f"{style.NONE} {style.YELLOW}"+json.loads(package.text)["errors"][0]["message"]+style.NONE)
+                return False, {}
+            elif json.loads(package.text)["data"]["trackings"][0]["shipment"]["shipmentId"] is None:
+                return False, {}
+            else:
+                return True, json.loads(package.text)
+
+        else:
+            return False, {}
+
+    @tree.command(name="track", description="Track package with auto updates in a specific channel.")
+    @discord.app_commands.describe(
+        tracking_number_list="The tracking numbers for the package to look for (separated by commas).",
+        channel ="The channel to send updates to.",
+        message ="The message to send along with the updates.",
+        timeout="The time in seconds between messages. (Default 30)."
+    )
+    async def first_command(interaction, tracking_number_list: str, channel: str, message:str, timeout:int = 3600):
+
+        trackLevel = 0
+
+        isDoneWithLastMessage = True
+
+        channel = int(channel)
+
+        try:
+            channel = bot.get_channel(channel)
+        except:
+            await interaction.response.send_message(content="**Invalid Channel**")
+            return
+
+        if timeout < 3600:
+            await interaction.response.send_message(content="**Timeout must be greater than or equal to 3600 seconds (1h)!**")
+            return
+
+
+        await interaction.response.send_message(content="‎")
+
+        while True:
+
+            trackLevel += 1
+
+            if isDoneWithLastMessage:
+                await channel.send(content=message)
+
+                current_index = 0
+
+                if "," in tracking_number_list:
+
+                    isDoneWithLastMessage = False
+
+                    tracking_numbers = tracking_number_list.split(",")
+
+                    for index in range(len(tracking_numbers)):
+
+                        current_index = index
+
+                        tracking_number = tracking_numbers[index]
+
+                        if len(tracking_number) >= 2:
+                            isValid, package_info = get_package(tracking_number)
+
+                            if isValid:
+                                package_info = package_info["data"]["trackings"][0]
+
+                                description = f"""
+                                
+                                **Estimated Delivery Date:** {str(package_info["shipment"]["delivery"]["estimatedDeliveryDate"])}
+
+                                **Recipient:**
+                                    > *Name:* {str(package_info["shipment"]["recipient"]["name"])}
+                                    > *Address:* {str(package_info["shipment"]["recipient"]["address"])}
+                                    > *Postal Code:* {str(package_info["shipment"]["recipient"]["postCode"])}
+                                    > *City:* {str(package_info["shipment"]["recipient"]["city"])}
+                                    > *State/Region:* {str(package_info["shipment"]["recipient"]["subdivision"])}
+
+                                **Service:** {str(package_info["shipment"]["delivery"]["service"])}
+                                """
+
+                                try:
+                                    footer = f"{str(package_info['events'][0]['sourceCode'])} | {str(package_info['shipment']['shipmentId'])}".upper()
+                                except:
+                                    footer = "No Info".upper()
+                                    pass
+
+                                def getColor():
+                                    try:
+                                        if (
+                                            str(package_info["shipment"]["statusMilestone"])
+                                            == "delivered"
+                                        ):
+                                            return 0x00FF00
+                                        elif (
+                                            str(package_info["shipment"]["statusMilestone"])
+                                            == "info_received"
+                                        ):
+                                            return 0x808080
+                                        elif (
+                                            str(package_info["shipment"]["statusMilestone"])
+                                            == "in_transit"
+                                        ):
+                                            return 0xFFFF00
+                                        else:
+                                            return 0xFF0000
+                                    except:
+                                        return 0xFF0000
+
+                                Embed = discord.Embed(
+                                    title=str(package_info["shipment"]["statusCategory"]).title()
+                                    + ": "
+                                    + tracking_number
+                                    + " | Status: "
+                                    + package_info["shipment"]["statusMilestone"].title(),
+                                    description=description,
+                                    color=getColor(),
+                                )
+
+                                Embed1 = discord.Embed(
+                                    title="Tracking Information: ", color=0x0000FF
+                                )
+                                Embed1.set_footer(text=footer)
+
+                                for event in package_info["events"]:
+                                    DT = str(event["occurrenceDatetime"])
+                                    TimeFormat = DT
+
+                                    try:
+                                        DT = DT.split("T")
+                                        Date = DT[0]
+                                        Time = DT[1]
+
+                                        TimeSplit = Time.split(":")
+
+                                        if int(TimeSplit[0]) > 12:
+                                            Tsub = int(TimeSplit[0]) - 12
+                                            Prefix = "PM"
+                                        else:
+                                            Tsub = int(TimeSplit[0])
+                                            Prefix = "AM"
+
+                                        TimeFormat = str(Tsub) + ":" + TimeSplit[1] + " " + Prefix
+
+                                        Embed1.add_field(
+                                            name=Date,
+                                            value=">>> "
+                                            + "**"
+                                            + TimeFormat
+                                            + " CDT**"
+                                            + "\n"
+                                            + str(event["status"]).title()
+                                            + "\n"
+                                            + str(event["location"]).upper(),
+                                            inline=False,
+                                        )
+
+                                    except:
+                                        Embed1.add_field(
+                                            name=Date,
+                                            value=">>> "
+                                            + "**"
+                                            + TimeFormat
+                                            + " CDT**"
+                                            + "\n"
+                                            + str(event["status"]).title()
+                                            + "\n"
+                                            + str(event["location"]).upper(),
+                                            inline=False,
+                                        )
+
+                                embedFinal = discord.Embed(color=0xffffff, title="Tracker Number #"+str(index+1), description="<:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667>")
+                                await channel.send(content="‎\n", embed=embedFinal)
+                                await channel.send(embed=Embed)
+                                await channel.send(embed=Embed1)
+
+                            else:
+                                Embed = discord.Embed(
+                                    title="Not Existent: " + tracking_number + " | Status: Invalid",
+                                    description="Invalid Shipment Code",
+                                    color=0xFF0000,
+                                )
+                                embedFinal = discord.Embed(color=0xffffff, title="Tracker Number #"+str(current_index+1), description="<:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667><:line:1112022903760879667>")
+                                await channel.send(content="‎\n", embed=embedFinal)
+                                await channel.send(embed=Embed)
+                    
+                    isDoneWithLastMessage = True
+
+                else:
+
+                    isDoneWithLastMessage = False
+
+                    tracking_number = tracking_number_list
+                    isValid, package_info = get_package(tracking_number)
+
+                    if isValid:
+                        package_info = package_info["data"]["trackings"][0]
+
+                        description = f"""
+                                
+                                **Estimated Delivery Date:** {str(package_info["shipment"]["delivery"]["estimatedDeliveryDate"])}
+
+                                **Recipient:**
+                                    > *Name:* {str(package_info["shipment"]["recipient"]["name"])}
+                                    > *Address:* {str(package_info["shipment"]["recipient"]["address"])}
+                                    > *Postal Code:* {str(package_info["shipment"]["recipient"]["postCode"])}
+                                    > *City:* {str(package_info["shipment"]["recipient"]["city"])}
+                                    > *State/Region:* {str(package_info["shipment"]["recipient"]["subdivision"])}
+
+                                **Service:** {str(package_info["shipment"]["delivery"]["service"])}
+                                """
+
+                        try:
+                            footer = f"{str(package_info['events'][0]['sourceCode'])} | {str(package_info['shipment']['shipmentId'])}".upper()
+                        except:
+                            footer = "No Info".upper()
+                            pass
+
+                        def getColor():
+                            try:
+                                if str(package_info["shipment"]["statusMilestone"]) == "delivered":
+                                    return 0x00FF00
+                                elif (
+                                    str(package_info["shipment"]["statusMilestone"])
+                                    == "info_received"
+                                ):
+                                    return 0x808080
+                                elif (
+                                    str(package_info["shipment"]["statusMilestone"]) == "in_transit"
+                                ):
+                                    return 0xFFFF00
+                                else:
+                                    return 0xFF0000
+                            except:
+                                return 0xFF0000
+
+                        Embed = discord.Embed(
+                            title=str(package_info["shipment"]["statusCategory"]).title()
+                            + ": "
+                            + tracking_number
+                            + " | Status: "
+                            + package_info["shipment"]["statusMilestone"].title(),
+                            description=description,
+                            color=getColor(),
+                        )
+
+                        Embed1 = discord.Embed(title="Tracking Information: ", color=0x0000FF)
+                        Embed1.set_footer(text=footer)
+
+                        for event in package_info["events"]:
+                            DT = str(event["occurrenceDatetime"])
+                            TimeFormat = DT
+
+                            try:
+                                DT = DT.split("T")
+                                Date = DT[0]
+                                Time = DT[1]
+
+                                TimeSplit = Time.split(":")
+
+                                if int(TimeSplit[0]) > 12:
+                                    Tsub = int(TimeSplit[0]) - 12
+                                    Prefix = "PM"
+                                else:
+                                    Tsub = int(TimeSplit[0])
+                                    Prefix = "AM"
+
+                                TimeFormat = str(Tsub) + ":" + TimeSplit[1] + " " + Prefix
+
+                                Embed1.add_field(
+                                    name=Date,
+                                    value=">>> "
+                                    + "**"
+                                    + TimeFormat
+                                    + " CDT**"
+                                    + "\n"
+                                    + str(event["status"]).title()
+                                    + "\n"
+                                    + str(event["location"]).upper(),
+                                    inline=False,
+                                )
+
+                            except:
+                                Embed1.add_field(
+                                    name=Date,
+                                    value=">>> "
+                                    + "**"
+                                    + TimeFormat
+                                    + " CDT**"
+                                    + "\n"
+                                    + str(event["status"]).title()
+                                    + "\n"
+                                    + str(event["location"]).upper(),
+                                    inline=False,
+                                )
+
+                        await channel.send(embed=Embed)
+                        await channel.send(embed=Embed1)
+
+                    else:
+                        Embed = discord.Embed(
+                            title="Tracking Number: " + tracking_number + " | Status: Invalid",
+                            description="Invalid Shipment Code",
+                            color=0xFF0000,
+                        )
+                        await channel.send(embed=Embed)
+
+                    isDoneWithLastMessage = True
+
+            time.sleep(timeout)
+            print(f"{style.GREEN}[TRACKING-{str(trackLevel)}]:{style.NONE} {style.MAGENTA}"+message+f"{style.NONE} {style.YELLOW}Last Updates Done: "+str(isDoneWithLastMessage)+style.NONE)
+
+    """
+
+    @bot.event
+    async def on_message(msg):
+        if "!locate" in msg.content:
+            message = str.split(msg.content, " ")
+            get_package(tracking_number=message[1])
+
+    """
+
+
+    @bot.event
+    async def on_ready():
+        await tree.sync()
+        print("Ready!")
+
+
+    bot.run(CONFIG["TOKEN"])
